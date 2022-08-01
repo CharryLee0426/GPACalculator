c3659b8366291f5ff59353be1e79c6d2d9e66ae9
lichen <lichen0426@stu.xidian.edu.cn>
2022-08-01 11:48:43 +0800

    🔧 UI design for IndexView

--- GPACalculator/Views/Index/IndexView.swift
+++ GPACalculator/Views/Index/IndexView.swift
@@ -3,14 +3,20 @@
 //  GPACalculator
 //
 //  Created by 李晨 on 2022/7/2.
-//
+//  Description: Index view shows index page after logining
 
 import SwiftUI
 
 struct IndexView: View {
+    @State var isLogined: Bool = false
+    
     var body: some View {
-        Text("Index View Shows the index page after logining")
-            .foregroundColor(.secondary)
+        if isLogined {
+            Text("You have been signed up.")
+                .foregroundColor(.secondary)
+        } else {
+            IndexWelcomeView()
+        }
     }
 }
 
