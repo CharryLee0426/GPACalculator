48a387e8be56c372610dbaec7bf8957b09a12535
lichen <lichen0426@stu.xidian.edu.cn>
2022-08-01 11:48:18 +0800

    🔧 UI design for IndexWelcomeView

--- GPACalculator/Views/Index/IndexWelcomeView.swift
+++ GPACalculator/Views/Index/IndexWelcomeView.swift
@@ -3,14 +3,30 @@
 //  GPACalculator
 //
 //  Created by 李晨 on 2022/7/2.
-//
+//  Description: Index Welcome View is used for noticing users to login
 
 import SwiftUI
 
 struct IndexWelcomeView: View {
     var body: some View {
-        Text("Index Welcome View is used for noticing users to login")
-            .foregroundColor(.secondary)
+        VStack(alignment: .center) {
+            HStack {
+                Spacer()
+                
+                Image(systemName: "questionmark.circle.fill")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(.blue)
+                
+                Spacer()
+            }
+            .padding()
+            
+            Text("It seems that you have not")
+                .font(.title)
+            Text("signed in")
+                .font(.title)
+        }
     }
 }
 
